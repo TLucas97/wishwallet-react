909dd383fb88179d8af217bdbf45831eedd0923f
Lucas Senna <tarcisio.almeida197@gmail.com>
2022-06-12 22:25:56 -0300

    fix

--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -9,7 +9,6 @@ import Box from '@mui/material/Box'
 import Button from '@mui/material/Button'
 import Typography from '@mui/material/Typography'
 import Modal from '@mui/material/Modal'
-import { fontWeight } from '@mui/system'
 
 const style = {
     position: 'absolute',
